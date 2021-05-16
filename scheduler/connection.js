9536b2e13d7a6bb39bba95218f595e468df17b3d
Brian Long <blong8334@gmail.com>
2021-05-16 16:10:33 -0400

    Finished scheduler

--- scheduler/connection.js
+++ scheduler/connection.js
@@ -10,6 +10,9 @@ class Connection {
   constructor() {
     this.connection = mysql.createConnection(options);
   }
+  escape(...args) {
+    return this.connection.escape(...args);
+  }
   connect() {
     return new Promise((resolve, reject) => {
       this.connection.connect((err) => {
